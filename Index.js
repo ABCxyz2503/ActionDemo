1891b50b968186419f70a787eff7f722fdf9d8aa
pradeepathirumurthi70-star <pradeepathirumurthi70@gmail.com>
2026-03-25 12:20:26 +0000

    Done some changes

---
+++ Index.js
@@ -0,0 +1,18 @@
+// Simple JavaScript example
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
+
+// Simple counter
+let count = 0;
+
+function increment() {
+    count++;
+    console.log(`Count: ${count}`);
+}
+
+increment();
+increment();
+increment();
